334f8bef8ac574aa1775666faa50f4ad411cce5a
Ahmad Naeem <ahmadnaeem.offcl@gmail.com>
2026-07-28 02:15:17 +0500

    dont know what hapened, but just to make sure everythong saves up

--- app/cleaning.py
+++ app/cleaning.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import streamlit as st
 
 def load_clean_data():
     df = pd.read_csv("data/processed/players_clean.csv")
-    return(df)
+    return df
 
-
-
+def load_css(path="assets/style.css"):
+    with open(path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
